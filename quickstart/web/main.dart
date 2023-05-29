48632e3f67772dcfe246ad50a8f4895547bc9d17
Warten0 Warten0 <1524763508@qq.com>
2023-05-29 01:34:04 +0000

    SAVE

---
+++ quickstart/web/main.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+Iterable<String> thingsTodo() sync* {
+  const actions = ['Walk', 'Wash', 'Feed'];
+  const pets = ['cats', 'dogs'];
+
+  for (final action in actions) {
+    for (final pet in pets) {
+      if (pet != 'cats' || action == 'Feed') {
+        yield '$action the $pet';
+      }
+    }
+  }
+}
+
+void addTodoItem(String item) {
+  final listElement = LIElement()..text = item;
+  todoList.children.add(listElement);
+}
+
+final UListElement todoList = querySelector('#todolist') as UListElement;
+
+void main() {
+  thingsTodo().forEach(addTodoItem);
+}
+
